51c6c4fa3de2d2382d19df88f96421fb6e668f14
Jay Lee <jay0lee@gmail.com>
2015-10-19 09:39:25 -0400

    describe how to run this command on a Chromebook

--- enable_rw_ssd.sh
+++ enable_rw_ssd.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+# Run this script on a Chromebook:
+# 1. Put Chromebook in developer mode - https://www.chromium.org/chromium-os/poking-around-your-chrome-os-device
+# 2. Log into device. Press CTRL+ALT+T to open crosh shell.
+# 3. Type "shell" to enter Bash shell.
+# 4. Type:
+#      bash <(curl -s -S -L https://raw.githubusercontent.com/jay0lee/cros-scripts/master/enable_rw_ssd.sh)
+
 sudo /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification --partitions "2 4"
 sudo mount -o remount,rw /
 if [ $? -ne 0 ]; then
