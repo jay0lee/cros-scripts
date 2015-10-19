8d454f68cdca6b29003a2173c70bee5515f9fb6a
Jay Lee <jay0lee@gmail.com>
2015-10-19 10:13:47 -0400

    confirm user is ok with recovery

--- enable_rw_ssd.sh
+++ enable_rw_ssd.sh
@@ -8,14 +8,34 @@
 # 4. Type:
 #      bash <(curl -s -S -L https://raw.githubusercontent.com/jay0lee/cros-scripts/master/enable_rw_ssd.sh)
 
-sudo /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification --partitions "2 4"
-sudo mount -o remount,rw /
-if [ $? -ne 0 ]; then
-  echo
-  echo
-  echo
-  echo "Reboot needed to enable OS writing. Please re-run script after a restart."
-  echo "Rebooting in 5 seconds..."
-  sleep 5
-  sudo reboot
+sudo touch /root-is-readwrite &> /dev/null
+if [ ! -f /root-is-readwrite ]
+then
+  while :
+    do
+      read -p "Modifying OS. USB Recovery will be needed to get out of developer mode. Are you sure you want to do this? (y/N): " is_user_sure
+      is_user_sure=${is_user_sure:0:1}
+      is_user_sure=${is_user_sure,,}
+      if [ ${is_user_sure} = "y" ]
+      then
+        break
+      else
+        echo "Come back when you're sure..."
+        exit
+      fi
+    done
+  sudo /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification --partitions "2 4"
+  sudo mount -o remount,rw /
+  if [ $? -ne 0 ]; then
+    echo
+    echo
+    echo
+    echo "Reboot needed to enable OS writing. Please re-run script after a restart."
+    echo "Rebooting in 5 seconds..."
+    sleep 5
+    sudo reboot
+  fi
+else
+  sudo rm -rf /root-is-readwrite
+  echo "Root filesystem is already read/write"
 fi
