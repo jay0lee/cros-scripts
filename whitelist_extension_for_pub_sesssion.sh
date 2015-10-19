c45af40f6f7a43f46a07806b9cef4f00a6dd9e02
Jay Lee <jay0lee@gmail.com>
2015-10-19 09:38:48 -0400

    describe how to run this command on a Chromebook

--- whitelist_extension_for_pub_sesssion.sh
+++ whitelist_extension_for_pub_sesssion.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Run this script on a Chromebook:
+# 1. Put Chromebook in developer mode - https://www.chromium.org/chromium-os/poking-around-your-chrome-os-device
+# 2. Log into device. Press CTRL+ALT+T to open crosh shell.
+# 3. Type "shell" to enter Bash shell.
+# 4. Type:
+#      bash <(curl -s -S -L https://raw.githubusercontent.com/jay0lee/cros-scripts/master/whitelist_extension_for_pub_sesssion.sh)
+
 function patch_strings_in_file() {
     local FILE="$1"
     local PATTERN="$2"
