0592ef4a062c00240e3ffa83e3d61284ad660afe
Jay Lee <jay0lee@gmail.com>
2016-08-26 11:43:30 -0400

    first shot

---
+++ recover-psks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Recover WPA PSKs - prints out PSKs for WPA-PSK networks pushed to device via user/device policy
+
+# Run this script on a Chromebook:
+# 1. Put Chromebook in developer mode - https://www.chromium.org/chromium-os/poking-around-your-chrome-os-device
+# 2. Log into device. Press CTRL+ALT+T to open crosh shell.
+# 3. Type "shell" to enter Bash shell.
+# 4. Type:
+#      bash <(curl -s -S -L https://raw.githubusercontent.com/jay0lee/cros-scripts/master/recover-psks.sh)
+
+echo "Device PSKs:"
+cat /var/cache/shill/default.profile | grep -E 'Name=|Profile=' | cut -d "=" -f 2- | while read line
+do
+  if [[ $line == "rot47:"* ]]
+  then
+    rotted=${line:6}
+    unrotted=`echo $rotted | tr '!-~' 'P-~!-O'`
+    echo " PSK:  $unrotted"
+  else
+    echo "SSID: $line"
+  fi
+done
