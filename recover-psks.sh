826c282eb8445c9f215eb216faa7e76179256f22
Jay Lee <jay0lee@gmail.com>
2016-08-26 12:07:27 -0400

    Update recover-psks.sh

--- recover-psks.sh
+++ recover-psks.sh
@@ -12,8 +12,7 @@
 echo "Device PSKs:"
 sudo cat /var/cache/shill/default.profile | grep -E 'Name=|Passphrase=' | cut -d "=" -f 2- | while read line
 do
-  if [[ "$line" =~ "^rot47.*" ]]
-  then
+  if [[ "$line" == "rot47"* ]]; then
     echo "psk"
     #rotted=${line:6}
     #unrotted=`echo $rotted | tr '!-~' 'P-~!-O'`
