c3d050fb012750f43af7ca03aa6acd51328c59b6
Jay Lee <jay0lee@gmail.com>
2016-08-26 11:54:08 -0400

    Update recover-psks.sh

--- recover-psks.sh
+++ recover-psks.sh
@@ -12,7 +12,7 @@
 echo "Device PSKs:"
 sudo cat /var/cache/shill/default.profile | grep -E 'Name=|Profile=' | cut -d "=" -f 2- | while read line
 do
-  if [[ $line == rot47:* ]]
+  if [[ $line == "rot47:*" ]]
   then
     rotted=${line:6}
     unrotted=`echo $rotted | tr '!-~' 'P-~!-O'`
