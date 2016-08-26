a40c858a65a3804c2498904addbbaf340ff68fc1
Jay Lee <jay0lee@gmail.com>
2016-08-26 13:35:35 -0400

    Update recover-psks.sh

--- recover-psks.sh
+++ recover-psks.sh
@@ -9,24 +9,29 @@
 # 4. Type:
 #      bash <(curl -s -S -L https://raw.githubusercontent.com/jay0lee/cros-scripts/master/recover-psks.sh)
 
+device_policy_file=/var/cache/shill/default.profile
 echo "Device PSKs:"
-sudo cat /var/cache/shill/default.profile | \
-grep -E '^Name=|^Passphrase=' | \
-cut -d "=" -f 2- | \
-while read line
-do
-  if [[ "$line" == "default" ]]; then
-    continue
-  fi
-  if [[ "$line" == "rot47"* ]]; then
-    rotted=${line:6}
-    unrotted=`echo $rotted | tr '!-~' 'P-~!-O'`
-    echo " PSK:  $unrotted"
-    echo
-  else
-    echo "SSID: $line"
-  fi
-done
+if ls $device_policy_file 1> /dev/null 2>&1; then
+  sudo cat $device_policy_file | \
+  grep -E '^Name=|^Passphrase=' | \
+  cut -d "=" -f 2- | \
+  while read line
+  do
+    if [[ "$line" == "default" ]]; then
+      continue
+    fi
+    if [[ "$line" == "rot47"* ]]; then
+      rotted=${line:6}
+      unrotted=`echo $rotted | tr '!-~' 'P-~!-O'`
+      echo " PSK:  $unrotted"
+      echo
+    else
+      echo "SSID: $line"
+    fi
+  done
+else
+  echo "No device policy networks found."
+fi
 echo
 echo
 echo "User PSKs:"
