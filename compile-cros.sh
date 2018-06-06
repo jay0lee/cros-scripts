a7dbb4d2f8a2ed70f157ebb8644b4b49207e5abf
Jay Lee <jay0lee@gmail.com>
2018-06-05 17:29:57 -0700

    create and use cros user

--- compile-cros.sh
+++ compile-cros.sh
@@ -10,6 +10,8 @@ else
   touch /root/.startup-script-ran
 fi
 
+useradd -m cros
+
 metadata_url="http://metadata.google.internal/computeMetadata/v1/instance/attributes/"
 board=`curl $metadata_url/board -H "Metadata-Flavor: Google"`
 branch=`curl $metadata_url/branch -H "Metadata-Flavor: Google"`
@@ -45,25 +47,25 @@ EOF
 chmod +x ./sudo_editor 
 sudo EDITOR=./sudo_editor visudo -f /etc/sudoers.d/relax_requirements
 
-sudo -i -u ubuntu git config --global color.ui false 
-sudo -i -u ubuntu git config --global user.email "jay0lee@gmail.com"
-sudo -i -u ubuntu git config --global user.name "Jay Lee"
+sudo -i -u cros git config --global color.ui false 
+sudo -i -u cros git config --global user.email "jay0lee@gmail.com"
+sudo -i -u cros git config --global user.name "Jay Lee"
 
-sudo -i -u ubuntu git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+sudo -i -u cros git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 
-sudo -i -u ubuntu mkdir -p chromiumos
+sudo -i -u cros mkdir -p chromiumos
 
-sudo -i -u ubuntu sh -c "cd chromiumos; /home/ubuntu/depot_tools/repo init \
+sudo -i -u cros sh -c "cd chromiumos; /home/cros/depot_tools/repo init \
   -u https://chromium.googlesource.com/chromiumos/manifest.git \
   --repo-url https://chromium.googlesource.com/external/repo.git -g minilayout $branch_flag"
-sudo -i -u ubuntu sh -c "cd chromiumos; /home/ubuntu/depot_tools/repo sync"
+sudo -i -u cros sh -c "cd chromiumos; /home/cros/depot_tools/repo sync"
 
-sudo -i -u ubuntu sh -c "cd chromiumos; /home/ubuntu/depot_tools/cros_sdk --download"
+sudo -i -u cros sh -c "cd chromiumos; /home/cros/depot_tools/cros_sdk --download"
 
-sudo -i -u ubuntu sh -c "cd chromiumos; /home/ubuntu/depot_tools/cros_sdk -- ./setup_board --board=$board --default"
+sudo -i -u cros sh -c "cd chromiumos; /home/cros/depot_tools/cros_sdk -- ./setup_board --board=$board --default"
 
-sudo -i -u ubuntu sh -c "cd chromiumos; /home/ubuntu/depot_tools/cros_sdk -- ./set_shared_user_password.sh chronos"
+sudo -i -u cros sh -c "cd chromiumos; /home/cros/depot_tools/cros_sdk -- ./set_shared_user_password.sh chronos"
 
-sudo -i -u ubuntu sh -c "cd chromiumos; /home/ubuntu/depot_tools/cros_sdk -- ./build_packages"
+sudo -i -u cros sh -c "cd chromiumos; /home/cros/depot_tools/cros_sdk -- ./build_packages"
 
-sudo -i -u ubuntu sh -c "cd chromiumos; cros_sdk -- ./build_image --noenable_rootfs_verification dev"
+sudo -i -u cros sh -c "cd chromiumos; cros_sdk -- ./build_image --noenable_rootfs_verification dev"
