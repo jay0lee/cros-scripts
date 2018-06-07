42d96b6ceb5636f38e3d03be7329d04dd950914e
Jay Lee <jay0lee@gmail.com>
2018-06-07 11:41:26 -0700

    Update compile-cros.sh

--- compile-cros.sh
+++ compile-cros.sh
@@ -20,11 +20,15 @@ then
   board="caroline"
 fi
 branch=`curl --fail $metadata_url/branch -H "Metadata-Flavor: Google"`
-branch_flag=""
 if [ ! -z  $branch ]
 then
   branch_flag="-b $branch"
+else
+  branch="master"
+  branch_flag=""
 fi
+
+echo "BUILDSCRIPT: building for board $board and branch $branch..."
 
 # start by making sure all installed packages
 # are up to date.
@@ -38,43 +42,34 @@ packages="git-core gitk git-gui curl lvm2 thin-provisioning-tools python-pkg-res
 echo "installing $packages"
 until apt -y install $packages
 do
-  echo "failed to install packages, sleeping and trying again"
+  echo "BUILDSCRIPT: failed to install packages, sleeping and trying again"
   sleep 10
   apt update
 done
 
-#cd /tmp
-#cat > ./sudo_editor <<EOF
-##!/bin/sh
-#echo Defaults \!tty_tickets > \$1          # Entering your password in one shell affects all shells 
-#echo Defaults timestamp_timeout=180 >> \$1 # Time between re-requesting your password, in minutes
-#EOF
-#chmod +x ./sudo_editor 
-#sudo EDITOR=./sudo_editor visudo -f /etc/sudoers.d/relax_requirements
-
 sudo -i -u cros bash << EOF
-  echo "setting git defaults..."
+  echo "BUILDSCRIPT: setting git defaults..."
   git config --global color.ui false 
   git config --global user.email "jay0lee@gmail.com"
   git config --global user.name "Jay Lee"
-  echo "cloning depot_tools..."
+  echo "BUILDSCRIPT: cloning depot_tools..."
   git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
   mkdir -p ~/chromiumos
   cd ~/chromiumos
-  echo "repo init..."
+  echo "BUILDSCRIPT: repo init..."
   /home/cros/depot_tools/repo init \
     -u https://chromium.googlesource.com/chromiumos/manifest.git \
     --repo-url https://chromium.googlesource.com/external/repo.git $branch_flag
-  echo "repo sync..."
+  echo "BUILDSCRIPT: repo sync..."
   /home/cros/depot_tools/repo sync -j8
-  echo "cros_sdk --download..."
+  echo "BUILDSCRIPT: cros_sdk --download..."
   /home/cros/depot_tools/cros_sdk --download
-  echo "setup_board..."
+  echo "BUILDSCRIPT: setup_board..."
   /home/cros/depot_tools/cros_sdk -- ./setup_board --board=$board --default
-  echo "set_shared_user_password..."
+  echo "BUILDSCRIPT: set_shared_user_password..."
   /home/cros/depot_tools/cros_sdk -- ./set_shared_user_password.sh chronos
-  echo "build_packages..."
+  echo "BUILDSCRIPT: build_packages..."
   /home/cros/depot_tools/cros_sdk -- ./build_packages --nowithdebug
-  echo "build_image..."
-  /home/cros/depot_tools/cros_sdk -- ./build_image dev
+  echo "BUILDSCRIPT: build_image..."
+  /home/cros/depot_tools/cros_sdk -- ./build_image --noenable_rootfs_verification dev
 EOF
