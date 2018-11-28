ec292d1a96ac1a44636be66a4513ad7fd2689a64
Jay Lee <jay0lee@gmail.com>
2018-11-28 13:54:47 -0500

    Create log-hids.sh

---
+++ log-hids.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Log HIDs - creates a /var/log/hids.log file with current list of USB devices every 1 sec
+
+# Run this script on a Chromebook:
+# 1. Put Chromebook in developer mode - https://www.chromium.org/chromium-os/poking-around-your-chrome-os-device
+# 2. Log into device. Press CTRL+ALT+T to open crosh shell.
+# 3. Type "shell" to enter Bash shell.
+# 4. Type:
+#      bash <(curl -s -S -L https://raw.githubusercontent.com/jay0lee/cros-scripts/master/log-hids.sh)
+
+# Make SSD read/write if it's not
+source <(curl -s -S -L https://raw.githubusercontent.com/jay0lee/cros-scripts/master/enable_rw_ssd.sh)
+
+sudo bash -c 'echo "script" > /etc/init/log_hids.conf'
+sudo bash -c 'echo "  watch -n1 \"date >> /var/log/hids.log; lsusb >> /var/log/hids.log; echo \'\' >> /var/log/hids.log\" \&" > /etc/init/log_hids.conf'
+sudo bash -c 'echo "end script" > /etc/init/log_hids.conf'
+echo
+echo "Enabled HID logging in /var/log/hids.log. Please reboot for logging to take effect."
